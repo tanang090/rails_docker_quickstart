8f80fee120d6d0f924c0abb9e3fcb75a04409304
tananag090 <tanaka086@gmail.com>
2018-10-18 13:34:32 +0900

    検証

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( show_view/answer)
 Rails.application.config.assets.precompile += %w( show_view/text)
+Rails.application.config.assets.precompile += %w( jquery.particleground.min.js demo.js)
